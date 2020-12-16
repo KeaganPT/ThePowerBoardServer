4392bbe13a35b0a1711f320beeba01aa78775145
Keagan Talbot <keaganpt@gmail.com>
2020-12-16 11:25:04 -0500

    forgot to sync

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json())
 
 app.use("/powers", controllers.powerscontroller)
 
+db.sync();
 app.listen(process.env.PORT, function() {
     console.log(`App is listening on ${process.env.PORT}`)
 })
