88ae986e5f7a0781325f2aef927feeca9df77fef
Keagan Talbot <keaganpt@gmail.com>
2021-01-14 15:55:35 -0500

    moved validate session call in user right above usage of it

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -4,7 +4,6 @@ const {User, Power, Character} = require('../models');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const validateSession = require('../middleware/validateSession');
 
 router.post('/register', (req,res) => {
     User.create({
@@ -16,49 +15,51 @@ router.post('/register', (req,res) => {
     .then(
         function userRegistered(user) {
             let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
-
+            
             res.json({
                 user: user,
                 message: "user created!",
                 sessionToken: token
             });
         }
-    )
-    .catch(err => res.status(500).json({ error: err}))
-});
-
-//Log in
-router.post('/login', function(req,res) {
-    console.log('I work heroku please')
-    User.findOne({
-        where: {
-            userName: req.body.userName
-        }
+        )
+        .catch(err => res.status(500).json({ error: err}))
+    });
+    
+    //Log in
+    router.post('/login', function(req,res) {
+        console.log('I work heroku please')
+        User.findOne({
+            where: {
+                userName: req.body.userName
+            }
+        })
+        .then(function userLoggedIn(user) {
+            if(user) {
+                bcrypt.compare(req.body.password, user.password, function (err, matches){
+                    if(matches) {
+                        let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
+                        res.status(200).json({
+                            user:user,
+                            message: "User logged in successfully",
+                            sessionToken: token
+                        })
+                    } else {
+                        res.status(502).send({ error: "Login failed"})
+                    }
+                });
+            } else {
+                res.status(500).json({ error: 'Cannot find user info'})
+            }
+        })
+        .catch(err => res.status(500).json({error: err}))
     })
-    .then(function userLoggedIn(user) {
-        if(user) {
-            bcrypt.compare(req.body.password, user.password, function (err, matches){
-                if(matches) {
-                    let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
-                    res.status(200).json({
-                        user:user,
-                        message: "User logged in successfully",
-                        sessionToken: token
-                    })
-                } else {
-                    res.status(502).send({ error: "Login failed"})
-                }
-            });
-        } else {
-            res.status(500).json({ error: 'Cannot find user info'})
-        }
-    })
-    .catch(err => res.status(500).json({error: err}))
-})
-
-router.get('/mine', validateSession, (req,res) => {
-    User.findOne({where: {id: req.user.id}, include:[{model: Power }, {model: Character}] })
-    .then(user => res.status(200).json(user))
+    
+    const validateSession = require('../middleware/validateSession');
+    
+    router.get('/mine', validateSession, (req,res) => {
+        User.findOne({where: {id: req.user.id}, include:[{model: Power }, {model: Character}] })
+        .then(user => res.status(200).json(user))
     .catch(err => res.status(500).json({ error: err}))
 })
 
